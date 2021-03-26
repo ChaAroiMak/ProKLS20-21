1b17e05bbec7a59f502af8eee57a85e00ccafe2b
ChaAroiMak <72292809+ChaAroiMak@users.noreply.github.com>
2021-03-26 09:08:49 +0100

    Add files via upload

--- ProKLS20_21/src/AutomatImpl.java
+++ ProKLS20_21/src/AutomatImpl.java
@@ -1,7 +1,14 @@
+import java.awt.desktop.SystemSleepEvent;
+import java.util.HashMap;
+
 public class AutomatImpl implements Automat{
+    AutomatImpl automat = new AutomatImpl();
+    HashMap<AutomatImpl, State> AUTOMAT = new HashMap<>();
+
     @Override
     public void start() {
-
+        setState(State.START);
+        AUTOMAT.put(automat, State.START);
     }
 
     @Override
@@ -25,10 +32,14 @@ public class AutomatImpl implements Automat{
 
     }
 
-    public State getState() {
-        return null;
+    public String getState() {
+        return AUTOMAT.toString();
     }
 
     public void end() {
+    }
+
+    public void setState(State state) {
+        this.setState(state);
     }
 }
